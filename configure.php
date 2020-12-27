2a8425ce57efe109418289096521085be0759aa5
A3h1nt <56585189+A3h1nt@users.noreply.github.com>
2020-12-27 15:32:16 +0530

    Update configure.php

--- configure.php
+++ configure.php
@@ -7,20 +7,60 @@
 
 <?php include'functions.php'; page_header(); ?>
 
-              <h2>Configure Proxy</a></h2>
-              <pre style="color: red">Works best with <a target="_blank" href="https://www.scraperapi.com">scrapeapi</a></pre>
-              <h1 style="color: lightgreen; text-align: right"><a href="index.php">New Scan</a></h1>
-              <form action="#" method="POST">
-                <input style="color: white" type="text" name="proxy" placeholder="Proxy URL eg: http://scraperapi:b8be80261es10c6277f49b1baf146a1d@proxy-server.scraperapi.com:8001 ">
-           <br>
-              <input style="background-color: grey;" type="submit" name="submit" value="submit">              
-                 </form>
-                 <?php
-                 showproxy();
-                 if(isset($_POST['proxy']))
-                 {
-                    $proxy_url = $_POST['proxy']; 
-                    addproxy($proxy_url);
-                 }
-                 ?>
+<h2>Configure Proxy</a></h2>
+<h1 style="color: lightgreen; text-align: right"><a href="index.php">New Scan</a></h1>
+<form action="#" method="POST">
+  <select style="color:white; background-color: #1e1f23" type="text" name="proxy" required>
+  <option value="select" selected>Select a proxy</option>
+  <option value="ScraperAPI">Scraper API<br>
+  <option value="ScrapingDog">Scraping Dog<br>
+  <option value="ZenScrape">Zen Scrape<br>
+    <option style="position: left">
+  </select>    
+  <br> 
+  <input style="color:white" type="text" name="api_key" placeholder="Enter the API key">
+  <br>      
+  <input style="background-color: grey;" type="submit" name="submit" value="submit"> 
+</form>
+<?php
+showproxy();
+error_reporting(0);
+if($_POST['submit'])
+{
+      if(!isset($_POST['api_key']))
+      {
+        echo "API key needed\n";
+        exit;
+      }
+      elseif($_POST['proxy'] == "ScraperAPI")
+      {
+        $proxy_url = "http://api.scraperapi.com?api_key=".$_POST['api_key']."&url=";
+        $test_url = "https://duckduckgo.com";
+        $name = "ScraperAPI";
+        validating_proxy($proxy_url,$test_url,$name);
+        echo $proxy_url;
+      }
+      elseif($_POST['proxy'] == "ScrapingDog")
+      {
+        $proxy_url = "https://api.scrapingdog.com/scrape?api_key=".$_POST['api_key']."&url=";
+        $test_url = "https://www.scrapingdog.com/";
+        $name = "ScrapingDog";
+        validating_proxy($proxy_url,$test_url,$name);
+        echo $proxy_url;
+      }
+      elseif($_POST['proxy'] == "ZenScrape")
+      {
+        $proxy_url = "https://app.zenscrape.com/api/v1/get?apikey=".$_POST['api_key']."&url=";
+        $test_url = "https://zenscrape.com/";
+        $name = "ZenScrape";
+        validating_proxy($proxy_url,$test_url,$name);
+        echo $proxy_url;
+      }
+      else
+      {
+        echo "No option Selected\n";
+        exit;
+      }
+}
+?>
 <?php footer(); ?>
